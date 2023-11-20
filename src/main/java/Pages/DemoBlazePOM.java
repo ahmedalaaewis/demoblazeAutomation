ae336f000ae79f0dbe02b4d5540efcfa340b9c0e
ahmedalaaewis <ahmedalaa4088@gmail.com>
2023-11-20 22:52:24 +0200

    Your commit message

--- src/main/java/Pages/DemoBlazePOM.java
+++ src/main/java/Pages/DemoBlazePOM.java
@@ -2,11 +2,10 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
+import UIElements.Button;
 
 public class DemoBlazePOM {
     private WebDriver driver;
@@ -17,11 +16,19 @@ public class DemoBlazePOM {
     private By phonesCategoryLocator = By.cssSelector("a#itemc[onclick*='phone']");
     private By cartLinkLocator = By.id("cartur");
 
+    // Initialize UI element classes
+    private Button laptopsCategoryButton;
+    private Button phonesCategoryButton;
+
     public DemoBlazePOM(WebDriver driver) {
         this.driver = driver;
 
         // Initialize the WebDriverWait with a 10-second timeout
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Initialize UI element classes
+        laptopsCategoryButton = new Button(driver, laptopsCategoryLocator);
+        phonesCategoryButton = new Button(driver, phonesCategoryLocator);
     }
 
     // Methods to interact with the page
@@ -31,34 +38,32 @@ public class DemoBlazePOM {
     }
 
     public void chooseLaptopsCategory() {
-        WebElement laptopsCategoryElement = wait.until(ExpectedConditions.elementToBeClickable(laptopsCategoryLocator));
-        laptopsCategoryElement.click();
+        laptopsCategoryButton.click();
     }
 
     public void choosePhonesCategory() {
-        WebElement phonesCategoryElement = wait.until(ExpectedConditions.elementToBeClickable(phonesCategoryLocator));
-        phonesCategoryElement.click();
+        phonesCategoryButton.click();
     }
 
     public void addToCart() {
         By addToCartButtonLocator = By.xpath("//a[contains(@onclick, 'addToCart') and contains(@class, 'btn-success')]");
-        WebElement addToCartElement = wait.until(ExpectedConditions.elementToBeClickable(addToCartButtonLocator));
-        addToCartElement.click();
+        Button addToCartButton = new Button(driver, addToCartButtonLocator);
+        addToCartButton.click();
     }
 
     public void viewCart() {
-        WebElement cartLinkElement = wait.until(ExpectedConditions.elementToBeClickable(cartLinkLocator));
-        cartLinkElement.click();
+        Button cartLinkButton = new Button(driver, cartLinkLocator);
+        cartLinkButton.click();
     }
 
     public String getCartLinkText() {
-        WebElement cartLinkElement = wait.until(ExpectedConditions.visibilityOfElementLocated(cartLinkLocator));
-        return cartLinkElement.getText();
+        Button cartLinkButton = new Button(driver, cartLinkLocator);
+        return cartLinkButton.getText();
     }
 
     public void selectProductByName(String productName) {
         By productLocator = By.xpath("//*[@id='tbodyid']/div[1]/div/div/h4/a[contains(text(), '" + productName + "')]");
-        WebElement productElement = wait.until(ExpectedConditions.elementToBeClickable(productLocator));
-        productElement.click();
+        Button productButton = new Button(driver, productLocator);
+        productButton.click();
     }
 }
